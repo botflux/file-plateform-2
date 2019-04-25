bce8690b338f970eedda7376fe4942fee039d62d
Victor Mendele <victor.mendele68@gmail.com>
2019-04-25 14:21:49 +0200

    :star2:feat: result view for character checker

---
+++ src/components/tool/character-checker/result.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-card>
+        <v-card-title>
+            <h2>Résultat</h2>
+        </v-card-title>
+
+        <v-data-table :headers="headers" :items="issues">
+            <template v-slot:items="props">
+                <td>{{ props.item.id }}</td>
+                <td>{{ props.item.line }}</td>
+                <td>{{ props.item.character }}</td>
+                <td>{{ props.item.sample }}</td>
+            </template>
+            <template v-slot:no-data>
+                Aucune erreur trouvé dans ce fichier
+            </template>
+        </v-data-table>
+
+        <v-card-actions>
+            <v-btn color="warning" @click="nextFile">Fichier suivant</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        issues: {
+            type: Array
+        },
+    },
+    data () {
+        return {      
+            headers: [
+                { text: 'ID', value: 'id' },
+                { text: 'Ligne', value: 'line' },
+                { text: 'Caractère', value: 'character' },
+                { text: 'Extrait', value: 'sample' }
+            ]
+        }
+    },
+    methods: {
+        nextFile () {
+            this.$emit('nextFile')
+        }
+    }
+}
+</script>
