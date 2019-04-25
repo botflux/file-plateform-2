8ef59fa8a9fdcdb44f9ab131003cb9ecaa696f1f
Victor Mendele <victor.mendele68@gmail.com>
2019-04-25 11:21:26 +0200

    :star2:feat: modal loader

---
+++ src/components/loader/modal-loader.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="text-cs-center">
+        <v-dialog v-model="isLoading" persistent width="300">
+            <v-card>
+                <v-card-text>
+                    Fichier en cours de traitement
+                    <v-progress-linear
+                        indeterminate
+                        color="primary"
+                        class="mb-0"
+                    ></v-progress-linear>
+                </v-card-text>
+            </v-card>
+        </v-dialog>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        isLoading: {
+            type: Boolean,
+            value: false
+        }
+    }
+}
+</script>
