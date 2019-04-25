5530c1ae9a5da88cd100cd5d3f1ab48bb637f7a0
Victor Mendele <victor.mendele68@gmail.com>
2019-04-25 11:21:10 +0200

    :star2:feat: tool header

---
+++ src/components/tool/header.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-flex>
+        <v-card>
+            <v-card-title>
+                <div>
+                    <span class="caption text-uppercase">{{ overline }}</span>
+                    <h1 class="display-1">{{ title }}</h1>
+                </div>
+            </v-card-title>
+            <v-card-text>{{ description }}</v-card-text>
+        </v-card>
+    </v-flex>
+</template>
+
+<script>
+export default {
+    props: {
+        overline: {
+            type: String,
+            value: 'overline'
+        }, 
+        title: {
+            type: String,
+            value: 'Titre'
+        }, 
+        description: {
+            type: String,
+            value: 'A description'
+        }
+    }
+}
+</script>
