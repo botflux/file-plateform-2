6b9fc613e06da2fb136b82eb13257bcf06948a1b
Victor Mendele <victor.mendele68@gmail.com>
2019-04-25 11:57:54 +0200

    :star2:feat: file input component

---
+++ src/components/form/file-input.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <input type="file" @change="changeFile($event)" class="hidden" ref="fileInput" />
+        <v-btn color="primary" @click="$refs.fileInput.click()">Choisissez un fichier</v-btn>
+        <span class="caption">{{ currentFilename }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        changeFile (e) {
+            this.$emit('input', e.target.files[0])
+        }
+    },
+    props: {
+        value: {
+            type: File,
+            value: null
+        }, 
+        noFileMessage: {
+            type: String,
+            value: 'Aucun fichier sélectionné'
+        },
+    },
+    computed: {
+        currentFilename () {
+            return this.file ? this.file.name : this.noFileMessage
+        },
+        file () {
+            return this.value
+        }
+    }
+}
+</script>
