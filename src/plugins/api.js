7d0e10cb32f1cae9769d71523549b43dec9b2fb2
Victor Mendele <victor.mendele68@gmail.com>
2019-05-02 09:34:06 +0200

    chore: add new api to API Plugin

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,12 +1,16 @@
 import { makeGetToken } from '../api/user'
 import { makeCheckCharacter } from '../api/character-checker'
+import { makeReadHeaders } from '../api/csv'
+import { makeCitiesExist } from '../api/cities'
 
 const APIPlugin = {}
 
 APIPlugin.install = (Vue, { getToken = () => '', baseUrl } = {}) => {
     Vue.prototype.$makeAPI = () => ({
         getToken: makeGetToken(window.fetch, { baseUrl }),
-        checkCharacter: makeCheckCharacter(window.fetch, { baseUrl, token: getToken() })
+        checkCharacter: makeCheckCharacter(window.fetch, { baseUrl, token: getToken() }),
+        readHeaders: makeReadHeaders(window.fetch, { baseUrl, token: getToken() }),
+        citiesExist: makeCitiesExist(window.fetch, { baseUrl, token: getToken() })
     })
 }
 
